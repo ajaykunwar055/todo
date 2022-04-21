4aae23a5c66c5d1a6fb41b5a91462fa448958729
Ajay Kunwar <ajaykunwar055@gmail.com>
2022-04-21 11:49:11 +0545

    commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <to_do />
+</template>
+
+<script>
+import to_do from './components/to_do.vue'
+
+export default {
+  name: 'App',
+  components: {
+    to_do
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/to_do.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <h1>Todo App</h1>
+        <input type="text" v-model="todo"/>  
+        <button @click="add">  Add</button>
+        <br>
+     
+     <ol>
+            <li v-for="todo in todos" :key="todo.list">
+        {{todo}} 
+            <button @click="removeTodo(index)">Delete</button>
+
+        <br>
+        </li>
+        </ol>
+    </div>
+</template>
+
+<script>
+export default {
+       data(){ 
+           return {
+           todo:'',
+           todos:['Learn Vue']
+        }
+       },
+       methods: {
+           add(){
+       this.todos.push(this.todo);
+       this.todo = ''
+       },
+         removeTodo(index) {
+         this.todos.splice(index, 1)
+                },
+       }
+}
+</script>
+
+<style>
+  
+</style>
